2de0567fe9a412e66fba76a4b23c361b3f76cfee
Sasha <sasha.yagupov1401@mail.ru>
2025-12-11 20:33:29 +0300

    2 player game (non complete)

--- Team_game_one.cpp
+++ Team_game_one.cpp
@@ -13,13 +13,16 @@ using namespace sf;
 #include "Flag.h"
 
 
-void menu(RenderWindow& window, Settings& mysettings, bool& gameStarted)
+void menu(RenderWindow& window, Settings& mysettings, int& gameStarted)
 {
     //кнопочки
     MyButton play_button(200, 50, Color::White, (mysettings.width - 200)/2, (mysettings.height - 50) * 0.3);
     play_button.setVisible(true);
 
-    MyButton exit_button(200, 50, Color::White, (mysettings.width - 200)/2, (mysettings.height - 50) * 0.425);
+    MyButton split_play(200, 50, Color::White, (mysettings.width - 200)/2, (mysettings.height - 50) * 0.425);
+    split_play.setVisible(true);
+
+    MyButton exit_button(200, 50, Color::White, (mysettings.width - 200) / 2, (mysettings.height - 50) * 0.550);
     exit_button.setVisible(true);
 
     //текстовые блоки
@@ -34,6 +37,11 @@ void menu(RenderWindow& window, Settings& mysettings, bool& gameStarted)
     play_text.setColor(Color::Black);
     play_text.center(play_button.get_shape().getGlobalBounds());
 
+    MyText splitplay_text("myfonts/arialmt.ttf", "Split");
+    splitplay_text.setVisible(true);
+    splitplay_text.setColor(Color::Black);
+    splitplay_text.center(split_play.get_shape().getGlobalBounds());
+
 
     MyText exit_text("myfonts/arialmt.ttf", "Exit");
     exit_text.setVisible(true);
@@ -43,17 +51,256 @@ void menu(RenderWindow& window, Settings& mysettings, bool& gameStarted)
 
     window.clear();
     play_button.draw(window);
+    split_play.draw(window);
     exit_button.draw(window);
 
     menu_text.draw(window);
     play_text.draw(window);
     exit_text.draw(window);
+    splitplay_text.draw(window);
 
     window.display();
 
-    if (play_button.is_pressed(window)) { gameStarted = true; }
+    if (play_button.is_pressed(window)) { gameStarted = 1; }
+    if (split_play.is_pressed(window)) { gameStarted = 2; }
     if (exit_button.is_pressed(window)) { window.close(); }
 
+}
+void splitGame(RenderWindow& window, Settings& mysettings) {
+    //для каждой игры свой игрок и 
+    const uint8_t mindist_to_flag = 12;
+
+    Player player;
+    Flag flag;
+
+    Player enemy;
+    Flag enemyflag;
+
+    uint16_t percent_of_resizing = (mysettings.height / 400 * 100);
+
+
+    float teamOneBaseX = 0;
+    float teamOneBaseY = mysettings.height / 2;
+
+    float teamTwoBaseX = mysettings.width;
+    float teamTwoBaseY = mysettings.height / 2;
+
+    /*============================ основной игрок =============================*/
+    // игрок
+    player.size = percent_of_resizing * player.size / 100;
+    player.speed = percent_of_resizing * player.speed / 100;
+    //float size = player.size; 
+
+    CircleShape player_circle(player.size / 2);
+    player.set_position(teamOneBaseX, teamOneBaseY);
+
+    player_circle.setPosition(player.x_pos, player.y_pos);
+    player_circle.setFillColor(Color::Red);
+    player_circle.setOutlineColor(Color::White);
+    player_circle.setOutlineThickness(player.size / 14);
+
+
+    // флажок
+    flag.size = percent_of_resizing * flag.size / 100;
+
+    CircleShape flag_circle(flag.size / 2);
+    flag.set_position(teamOneBaseX, teamOneBaseY);
+
+    flag_circle.setPosition(flag.x_pos, flag.y_pos);
+    flag_circle.setFillColor(Color::Green);
+    flag_circle.setOutlineColor(Color::Red);
+    flag_circle.setOutlineThickness(flag.size / 7);
+    /*=========================================================*/
+
+    /*============================ второстепенный игрок =============================*/
+    // игрок
+    enemy.size = percent_of_resizing * enemy.size / 100;
+    enemy.speed = percent_of_resizing * enemy.speed / 100;
+    //float size = player.size; 
+
+    CircleShape enemy_circle(enemy.size / 2);
+    enemy.set_position(teamTwoBaseX - enemy.size, teamTwoBaseY);
+
+    enemy_circle.setPosition(enemy.x_pos, enemy.y_pos);
+    enemy_circle.setFillColor(Color::Blue);
+    enemy_circle.setOutlineColor(Color::White);
+    enemy_circle.setOutlineThickness(enemy.size / 14);
+
+
+    // флажок
+    enemyflag.size = percent_of_resizing * enemyflag.size / 100;
+
+    CircleShape enemyflag_circle(enemyflag.size / 2);
+    enemyflag.set_position(teamTwoBaseX - enemy.size, teamTwoBaseY);
+
+    enemyflag_circle.setPosition(enemyflag.x_pos, enemyflag.y_pos);
+    enemyflag_circle.setFillColor(Color::Green);
+    enemyflag_circle.setOutlineColor(Color::Blue);
+    enemyflag_circle.setOutlineThickness(enemyflag.size / 7);
+    /*=========================================================*/
+
+    //текстовые блоки
+    MyText score1_text("myfonts/arial_bolditalicmt.ttf", "Score:", 28);
+    score1_text.setVisible(true);
+    score1_text.setColor(Color::Red);
+    score1_text.setPosition((mysettings.width - 200) * 0.1, 0);
+
+    MyText score1_value("myfonts/arialmt.ttf", "0", 24);
+    score1_value.setVisible(true);
+    score1_value.setColor(Color::Red);
+    score1_value.setPosition((mysettings.width - 200) * 0.1 + 100, 0);
+
+
+
+    // Автоматический расчет размера тайлов относительно экрана
+    const int mapWidthTiles = 24;   // количество тайлов по ширине
+    const int mapHeightTiles = 16;  // количество тайлов по высоте
+
+
+    int tileSize = min(mysettings.width / mapWidthTiles, mysettings.height / mapHeightTiles);
+
+    GameMap gameMap(mapWidthTiles, mapHeightTiles, tileSize);
+
+    if (!gameMap.loadFromFile("maps/fastrunner.txt", Color::White)) {
+        cout << "error: map not loaded!" << endl;
+    }
+
+    bool gameRun = true;
+    uint8_t speed = player.speed; //так то игроки на равных 
+
+    while (gameRun)
+    {
+        // Сохраняем старую позицию для отката при коллизии
+        float oldX = player.x_pos;
+        float oldY = player.y_pos;
+
+        float oldX2 = enemy.x_pos;
+        float oldY2 = enemy.y_pos;
+
+
+        if (Keyboard::isKeyPressed(Keyboard::A)) {
+            player.move(-speed, 0);
+            player_circle.move(-speed, 0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::D)) {
+            player.move(speed, 0);
+            player_circle.move(speed, 0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::W)) {
+            player.move(0, -speed);
+            player_circle.move(0, -speed);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::S)) {
+            player.move(0, speed);
+            player_circle.move(0, speed);
+        }
+
+        //2nd player
+        if (Keyboard::isKeyPressed(Keyboard::Left)) {
+            enemy.move(-speed, 0);
+            enemy_circle.move(-speed, 0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Right)) {
+            enemy.move(speed, 0);
+            enemy_circle.move(speed, 0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Up)) {
+            enemy.move(0, -speed);
+            enemy_circle.move(0, -speed);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Down)) {
+            enemy.move(0, speed);
+            enemy_circle.move(0, speed);
+        }
+
+        int distance;
+        if (Keyboard::isKeyPressed(Keyboard::E)) {
+            distance = sqrt(pow((enemyflag.x_pos - player.x_pos), 2) + pow((enemyflag.y_pos - player.y_pos), 2));
+            cout << distance << endl;
+            player.flag = (distance < mindist_to_flag);
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::RControl)) {
+            distance = sqrt(pow((flag.x_pos - enemy.x_pos), 2) + pow((flag.y_pos - enemy.y_pos), 2));
+            cout << distance << endl;
+            enemy.flag = (distance < mindist_to_flag);
+        }
+
+        // Проверяем коллизии
+        if (gameMap.checkCollision(player.x_pos, player.y_pos, player.size) ||
+            player.x_pos < 0 || player.x_pos > mysettings.width - player.size ||
+            player.y_pos < 0 || player.y_pos > mysettings.height - player.size) {
+
+            // Откатываем позицию
+            player.set_position(oldX, oldY);
+            player_circle.setPosition(oldX, oldY);
+        }
+
+        if (gameMap.checkCollision(enemy.x_pos, enemy.y_pos, enemy.size) ||
+            enemy.x_pos < 0 || enemy.x_pos > mysettings.width - enemy.size ||
+            enemy.y_pos < 0 || enemy.y_pos > mysettings.height - enemy.size) {
+
+            // Откатываем позицию
+            enemy.set_position(oldX2, oldY2);
+            enemy_circle.setPosition(oldX2, oldY2);
+        }
+
+
+
+        //exit to menu
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            player.set_position(0, 0);
+            gameRun = false;
+        }
+
+        //move flag
+        if (player.flag) {
+            enemyflag.set_position(player.x_pos, player.y_pos);
+            enemyflag_circle.setPosition(player.x_pos, player.y_pos);
+        }
+
+        if (enemy.flag) {
+            flag.set_position(enemy.x_pos, enemy.y_pos);
+            flag_circle.setPosition(enemy.x_pos, enemy.y_pos);
+        }
+
+        distance = sqrt(pow((enemyflag.x_pos - teamOneBaseX), 2) + pow((enemyflag.y_pos - teamOneBaseY), 2));
+        if (distance < mindist_to_flag) {
+            enemyflag.set_position(teamTwoBaseX - enemyflag.size, teamTwoBaseY);
+            enemyflag_circle.setPosition(teamTwoBaseX - enemyflag.size, teamTwoBaseY);
+
+            player.score += 1;
+            player.flag = false;
+        }
+
+        distance = sqrt(pow((flag.x_pos - teamTwoBaseX), 2) + pow((flag.y_pos - teamTwoBaseY), 2));
+        if (distance < mindist_to_flag) {
+            flag.set_position(teamOneBaseX, teamOneBaseY);
+            flag_circle.setPosition(teamOneBaseX, teamOneBaseY);
+
+            enemy.score += 1;
+            enemy.flag = false;
+        }
+
+
+        window.clear();
+
+
+        gameMap.draw(window);
+        window.draw(player_circle);
+        window.draw(flag_circle);
+
+        window.draw(enemy_circle);
+        window.draw(enemyflag_circle);
+        
+
+        score1_text.draw(window);
+        score1_value.setString(to_string(player.score));
+        score1_value.draw(window);
+
+
+        window.display();
+    }
 }
 
 void singleGame(RenderWindow& window, Settings& mysettings) {
@@ -224,10 +471,10 @@ void singleGame(RenderWindow& window, Settings& mysettings) {
 
         gameMap.draw(window);
         window.draw(player_circle);
-        window.draw(flag_circle);
+        window.draw(enemy_circle);
 
         window.draw(enemyflag_circle);
-        window.draw(enemy_circle);
+        window.draw(flag_circle);
 
         score1_text.draw(window);
         score1_value.setString(to_string(player.score));
@@ -255,7 +502,7 @@ int main()
     /* =============================== */
   
 
-    bool gameStarted = false;
+    int gameStarted = 0;
 
     while (window.isOpen())
     {
@@ -267,8 +514,12 @@ int main()
         }
 
         //menu
-        if (gameStarted) {
+        if (gameStarted==1) {
             singleGame(window, mysettings); 
+            gameStarted = false;
+        }
+        if (gameStarted == 2) {
+            splitGame(window, mysettings);
             gameStarted = false;
         }
         menu(window, mysettings, gameStarted);
